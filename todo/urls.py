f18c8f47232685428b50658382b4f0ca7fcae0d8
aakash <aakashy613@gmail.com>
2023-04-20 13:16:07 +0530

    added docs

--- todo/urls.py
+++ todo/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from . import views
 
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
     path("task-create/", views.todocreate, name="create"),
     path("task-update/<str:pk>/", views.todoupdate, name="update"),
     path("task-delete/<str:pk>/", views.tododelete, name="delete"),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from drf_spectacular.utils import extend_schema
 from .serializers import TodoSerializers
 from .models import Todo
-
 
 # Create your views here.
 
@@ -11,16 +11,16 @@ from .models import Todo
 @api_view(['GET'])
 def apioverview(request):
     api_urls = {
-        'List':'/task-list/',
-        'Detail View':'/task-detail/<str:pk>/',
-        'Create':'/task-create/',
-        'Update':'/task-update/<str:pk>/',
-        'Delete':'/task-delete/<str:pk>/'
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/'
     }
-
     return Response(api_urls)
 
 
+@extend_schema(responses=TodoSerializers)
 @api_view(['GET'])
 def todo(request):
     task = Todo.objects.all()
